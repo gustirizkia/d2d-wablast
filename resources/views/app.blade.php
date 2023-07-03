aef4e97e0aeff96742289e426ef8e7df6af2fe3f
gustirizkia <gustidoang936@gmail.com>
2023-07-03 21:44:09 +0700

    change data

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.jsx?version=002', "resources/js/Pages/{$page['component']}.jsx?version=002"])
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
