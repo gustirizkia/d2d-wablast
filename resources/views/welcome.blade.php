55769aed9f6fd29616e7c8e6c9eca3d4060a7126
gustirizkia <gustidoang936@gmail.com>
2023-07-22 13:18:49 +0700

    change page login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                 >
                   <a
                     class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder"
-                    href="{{ route('login') }}"
+                    href="/login"
                     >Login</a
                   >
                   <a
