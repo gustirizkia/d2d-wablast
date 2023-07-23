b0dac06f160fabd1ec35fbdb743a8010f8c48eac
Yoga Febriatala <yogafebriatala24@gmail.com>
2023-07-23 19:17:36 +0700

    change name

---
+++ resources/views/pages/Realcount/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin')
+
+@section('title')
+    Realcount
+@endsection
+
+@section('content')
+    <div class="card" style="min-height: 100vh">
+        <div class="card-body">
+            <div class="text-center">
+                <h1 class="h2">Coming Soon</h1>
+            </div>
+        </div>
+    </div>
+@endsection
